ac33673a8ceca282bf267c1aea5a66e018ba3257
Kevin Philip <kevinphilip2004@gmail.com>
2024-10-01 00:01:00 -0500

    configured entryPoint
    We now have an entry point in which you can pass an arg into the GPT prompt, it now gives a sentence back depending on the number of words you give. Example use 'gptImplementation.py -i 10' will give a sentence with 10 words

--- gptImplementation.py
+++ gptImplementation.py
@@ -1,6 +1,10 @@
 import os
+# imports for the ai
 from openai import OpenAI
 from dotenv import load_dotenv
+
+#imports for the main initalization
+import argparse
 
 load_dotenv()
 
@@ -11,10 +15,9 @@ client = OpenAI(
 
 
 # Below is configuring the prompt you want to send GPT 3
-numberOfWords = "15"
-prompt = f"Give me a random sentence containing {numberOfWords} words"
-
-response = client.chat.completions.create(
+def useGPT(argument):
+    prompt = f"Give me a random sentence containing {argument} words"
+    response = client.chat.completions.create(
     messages=[
         {
             "role": "user",
@@ -22,6 +25,28 @@ response = client.chat.completions.create(
         }
     ],
     model="gpt-3.5-turbo",
-)
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
+
+# 2) This is the main function that takes retrieves inputs using argparse library
+# The argument is then passed into gpt to get a response
+def main():
+    print("Entry Point Configured")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", "-i", type=str, required=True)
+    args = parser.parse_args()
+    userInput = args.input
+
+    print(useGPT(userInput))
+
+    pass
+
+
+# 1) This is what sets the entry points
+# When the app is ran as 'python gptImplementation.py' for example, 
+# the var __name__ is auto'd to __main__, so we can check then and execute functions from it
+if __name__ == "__main__":
+    main()
