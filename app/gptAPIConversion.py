cd58cf571d7be017d30a467747a931f47cdfb38e
Kevin Philip <kevinphilip2004@gmail.com>
2024-10-18 17:38:53 -0500

    checkpoint commit

--- app/gptAPIConversion.py
+++ app/gptAPIConversion.py
@@ -1,4 +1,3 @@
-from typing import Union
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,17 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Lambda } from "aws-cdk-lib/aws-ses-actions";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
+    });
+
     const apiLambda = new lambda.Function(this, "ApiFunction", {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset("../app/"),
       handler: "gptAPIConversion.handler",
+      layers: [layer],
     });
   }
 }
